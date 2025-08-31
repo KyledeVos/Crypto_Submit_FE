0aefadd1e931c22099b0651ef30391f5cf8bd624
Kyle <kdevos827@gmail.com>
2025-08-31 17:16:18 +0200

    Data Display table modified for row highlight on hover

--- src/components/elements/dataDisplayTable.tsx
+++ src/components/elements/dataDisplayTable.tsx
@@ -3,9 +3,11 @@ import type { dataDisplayTableType } from "../../types/element_types";
 import type { CryptoSummaryTableType } from "../../types/model_types";
 
 const DataDisplayTable = ({
+  tableHeading,
   headings,
   data,
   onRowSelect,
+  clickableRow,
 }: dataDisplayTableType) => {
   const [dataEmpty, setDataEmpty] = useState<boolean>(false);
 
@@ -22,7 +24,14 @@ const DataDisplayTable = ({
 
   return (
     <>
-      <table className="table bg-dark mt-5">
+      {tableHeading && (
+        <div className="row w-full justify-content-center">
+          <h2 className="heading_font">{tableHeading}</h2>
+        </div>
+      )}
+      <table
+        className={`table ${clickableRow ? "table-hover" : ""} bg-dark mt-1`}
+      >
         <thead className="table-dark">
           <tr>
             {headings.map((heading) => {
@@ -31,19 +40,25 @@ const DataDisplayTable = ({
           </tr>
         </thead>
         <tbody className="table-dark">
-          {data.map((currentData) => {
-            return (
-              <tr
-                onClick={() => {
-                  onRowSelect((currentData as CryptoSummaryTableType).symbol);
-                }}
-              >
-                {Object.values(currentData).map((field) => {
-                  return <td>{field}</td>;
-                })}
-              </tr>
-            );
-          })}
+          {data.length > 0 ? (
+            data.map((currentData) => {
+              return (
+                <tr role={clickableRow ? "button" : undefined}
+                  onClick={() => {
+                    onRowSelect((currentData as CryptoSummaryTableType).symbol);
+                  }}
+                >
+                  {Object.values(currentData).map((field) => {
+                    return <td>{field}</td>;
+                  })}
+                </tr>
+              );
+            })
+          ) : (
+            <tr className="text-center">
+              <td colSpan={headings.length}>NO DATA</td>
+            </tr>
+          )}
         </tbody>
       </table>
     </>
--- src/types/element_types.ts
+++ src/types/element_types.ts
@@ -1,12 +1,14 @@
 export type customToastType = {
-    toastType: 'alert' | 'info'| 'success';
+    toastType: 'alert' | 'info' | 'success';
     message: string;
     showToast: boolean;
     onClose: () => void
 }
 
 export type dataDisplayTableType = {
+    tableHeading?: string;
     headings: string[];
     data: object[];
-    onRowSelect : (symbol: string) => void
+    onRowSelect: (symbol: string) => void
+    clickableRow?:boolean
 }
